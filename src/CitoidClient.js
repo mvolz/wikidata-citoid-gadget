964738a1d93446baf6b525ed815a29acb24fbbbf
mvolz <marielle.volz@gmail.com>
2018-11-13 11:49:37 +0000

    Clean up CitoidClient.js
    Remove redundant Promise as $.ajax already
    returns a Promise natively.

--- src/CitoidClient.js
+++ src/CitoidClient.js
@@ -7,20 +7,14 @@ function CitoidClient() {
 }
 
 CitoidClient.prototype.search = function( value ) {
-    var dfd = $.Deferred(),
-        baseUrl = 'https://en.wikipedia.org/api/rest_v1/data/citation',
+    var baseUrl = 'https://en.wikipedia.org/api/rest_v1/data/citation',
         format = 'mediawiki-basefields',
         url = baseUrl + '/' + format + '/' + encodeURIComponent(value);
-    $.ajax( {
-        method: 'GET',
-        url: url,
-        data: {}
-    } )
-    .done( function( citoidData ) {
-        dfd.resolve( citoidData );
-    } );
 
-    return dfd.promise();
+    return $.ajax( {
+        method: 'GET',
+        url: url
+    } );
 };
 
 mw.CitoidClient = CitoidClient;
