61475eec0696715666078733ff0b93150444f4ae
mvolz <marielle.volz@gmail.com>
2018-11-22 09:48:22 +0000

    Add error message to pending dialog
    Add error message to pending dialog
    when citoid is unable to get results
    (previously would stay pending
    indefinitely!)

--- src/CiteToolAutofillLinkRenderer.js
+++ src/CiteToolAutofillLinkRenderer.js
@@ -11,26 +11,46 @@ function CiteToolAutofillLinkRenderer( config, citoidClient, citeToolReferenceEd
 }
 
 CiteToolAutofillLinkRenderer.prototype.renderLink = function( referenceView ) {
-    var self = this;
+	var self = this;
 
-    var options = {
-        templateParams: [
-            'wikibase-citetool-autofill wikibase-referenceview-autofill', // CSS class names
-            '#', // URL
-            'autofill', // Label
-            '' // Title tooltip
-        ],
-        cssClassSuffix: 'autofill'
-    };
+	var options = {
+		templateParams: [
+			'wikibase-citetool-autofill wikibase-referenceview-autofill', // CSS class names
+			'#', // URL
+			'autofill', // Label
+			'' // Title tooltip
+		],
+		cssClassSuffix: 'autofill'
+	};
 
-    var $span = $( '<span/>' )
-        .toolbarbutton( options )
-        .on( 'click', function( e ) {
-            e.preventDefault();
-            self.onAutofillClick( e.target );
-        } );
+	var $span = $( '<span/>' )
+		.toolbarbutton( options )
+		.on( 'click', function( e ) {
+			e.preventDefault();
+			self.onAutofillClick( e.target );
+		} );
 
-    this.getReferenceToolbarContainer( referenceView ).append( $span );
+	this.getReferenceToolbarContainer( referenceView ).append( $span );
+};
+
+CiteToolAutofillLinkRenderer.prototype.disableLink = function( target ){
+	var buttons = $( target ).find( '.wikibase-citetool-autofill' );
+	var button;
+
+	if ( buttons.length > 0 ){
+		button = buttons[0];
+		button.toolbarbutton.disable();
+	}
+};
+
+CiteToolAutofillLinkRenderer.prototype.enableLink = function( target ){
+	var buttons = $( target ).find( '.wikibase-citetool-autofill' );
+	var button;
+
+	if ( buttons.length > 0 ){
+		button = buttons[0];
+		button.toolbarbutton.enable();
+	}
 };
 
 CiteToolAutofillLinkRenderer.prototype.getReferenceFromView = function( referenceView ) {
@@ -92,16 +112,26 @@ CiteToolAutofillLinkRenderer.prototype.onAutofillClick = function( target ) {
 
 	self.windowManager.openWindow( self.pendingDialog );
 	self.pendingDialog.pushPending();
+	self.pendingDialog.getActionProcess.call( self.pendingDialog, 'waiting' );
 
 	this.citoidClient.search( value )
-		.done( function( data ) {
-			if ( data[0] ) {
-				self.citeToolReferenceEditor.addReferenceSnaksFromCitoidData(
-					data[0],
-					referenceView
-				);
+		.then( 
+			//success
+			function( data ) {
+				if ( data[0] ) {
+					self.citeToolReferenceEditor.addReferenceSnaksFromCitoidData(
+						data[0],
+						referenceView
+					);
+				}
+			},
+			//failure
+			function() {
+				self.pendingDialog.popPending();
+				self.pendingDialog.getActionProcess.call( self.pendingDialog, 'error' );
 			}
-		} );
+
+		);
 };
 
 CiteToolAutofillLinkRenderer.prototype.getLookupSnakValue = function( reference ) {
--- src/PendingDialog.js
+++ src/PendingDialog.js
@@ -12,26 +12,56 @@ PendingDialog.static.name = 'PendingDialog';
 // Specify a title statically (or, alternatively, with data passed to the opening() method).
 PendingDialog.static.title = 'Generating Citation';
 
+PendingDialog.static.actions = [
+  { action: 'waiting', modes: 'edit', label: 'Searching' },
+  { action: 'error', modes: 'edit', label: 'Error' }
+];
+
+
+
 // Customize the initialize() function: This is where to add content to the dialog body and set up event handlers.
 PendingDialog.prototype.initialize = function () {
   // Call the parent method
   PendingDialog.super.prototype.initialize.apply( this, arguments );
   // Create and append a layout and some content.
-  this.content = new OO.ui.PanelLayout( { 
-    padded: true,
-    expanded: false 
-  } );
-  this.content.$element.append( '<p>Generating a reference for you.... please wait...</p>' );
-  this.$body.append( this.content.$element );
+
+  this.panel1 = new OO.ui.PanelLayout( { padded: true, expanded: false } );
+  this.panel1.$element.append( '<p>Generating a reference for you.... please wait...</p>' );
+  this.panel2 = new OO.ui.PanelLayout( { padded: true, expanded: false } );
+  this.panel2.$element.append( '<p>Unable to generate reference from this input. Press escape to exit.</p>' );
+
+  this.stackLayout= new OO.ui.StackLayout( {
+    items: [ this.panel1, this.panel2 ]
+  } ); 
+
+  this.stackLayout.setItem( this.panel1 );
+  this.$body.append( this.stackLayout.$element );    
+
 };
+
+PendingDialog.prototype.getSetupProcess = function ( data ) {
+  return PendingDialog.super.prototype.getSetupProcess.call( this, data )
+  .next( function () {
+    this.actions.setMode( 'waiting' );
+  }, this );
+};
+
 
 // Specify processes to handle the actions.
 PendingDialog.prototype.getActionProcess = function ( action ) {
-  if ( action === 'open' ) {
-    // Create a new process to handle the action
-    return new OO.ui.Process( function () {
-      setTimeout(function(){ }, 3000); // SOOOO HACKY 
-    }, this );
+  if ( action === 'waiting' ) {
+    //this.pushPending();
+    // Set the mode to help.
+    this.actions.setMode( 'waiting' );
+    // Show the help panel.
+    this.stackLayout.setItem( this.panel1 );
+  }
+  if ( action === 'error' ) {
+    //this.popPending();
+    // Set the mode to help.
+    this.actions.setMode( 'error' );
+    // Show the help panel.
+    this.stackLayout.setItem( this.panel2 );
   }
   // Fallback to parent handler
   return PendingDialog.super.prototype.getActionProcess.call( this, action );
@@ -40,9 +70,10 @@ PendingDialog.prototype.getActionProcess = function ( action ) {
 
 // Override the getBodyHeight() method to specify a custom height (or don't to use the automatically generated height)
 PendingDialog.prototype.getBodyHeight = function () {
-  return this.content.$element.outerHeight( true );
+  return this.panel1.$element.outerHeight( true );
 };
 
 mw.PendingDialog = PendingDialog;
 
 }( mediaWiki, jQuery ) );
+  
